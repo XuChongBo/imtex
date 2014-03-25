b2d76002a624c3aeb7a753304e35e932c17daed8
XuChongBo <xcbfreedom@gmail.com>
2014-03-25 12:53:49 +0800

    get_word_centroid_points function

--- do.py
+++ do.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from pylab import imshow,show,subplot,array,figure,gray,uint8,hist
+from pylab import imshow,show,subplot,array,figure,gray,uint8,hist,plot
 import numpy as np
 from pylab import jet
 
@@ -7,40 +7,50 @@ from skimage import filter
 from scipy.ndimage import filters,measurements
 from scipy import stats
 
+#showFigure=True
+showFigure=False
 
-if __name__=="__main__":
-    #file_name='./data/das-0.jpg'
-    #file_name='./data/EngBill21.jpg'
-    #file_name='./data/sample1.jpg'
-    file_name='/Users/xcbfreedom/projects/data/formula_images/user_images/531283fa24f0b8afb.png'
-    # load the image file
-    img = Image.open(file_name)
-    print img
-
-    #======= find word regions ========
+def get_word_centroid_points(img):
+    """
+        find the connected component who's area satisfy some condition, which can be identified to be word regions.
+        
+        input: img
+        output:
+             (word_centroid_y_list,word_centroid_x_list)
+    """
+    #==== preprocess 
     # convert to gray
     img_gray = array(img.convert('L'))   # not inplace operator
     img_gray = 255-img_gray
 
+    if showFigure:
+        figure(); gray(); # don't use colors 
+        imshow(img_gray)
+        show()
     # binary
     #img_bin = filter.threshold_adaptive(img_gray,17,method='mean')
     global_thresh = filter.threshold_otsu(img_gray)
     img_bin = img_gray > global_thresh 
 
-    # find connect components 
-    s = array([[1,1,1],[1,1,1],[1,1,1]])
+    if showFigure:
+        figure(); gray(); # don't use colors 
+        imshow(img_bin)
+        show()
 
+    #== find connect components 
+    s = array([[1,1,1],[1,1,1],[1,1,1]])
     # the mask image and num of objects
     labeled_array, num_features = measurements.label(img_bin, structure=s)
     print 'num of labels:', num_features 
-
-
-    ob_area_list = []
-    #for ob in obj_list:
-    #h = ob[0].stop-ob[0].start
-    #w = ob[1].stop-ob[1].start
-    #print ob, h, w
-    img_bin_words = np.zeros_like(img_bin)
+    if showFigure:
+        figure(); gray(); # don't use colors 
+        imshow(labeled_array)
+        jet()
+        show()
+    
+    #== filter the connected component by area
+    word_area_list = []
+    word_label_array = np.zeros_like(labeled_array)
     word_label_list = []
     for i in range(num_features):
         mask_value=i+1
@@ -48,13 +58,18 @@ if __name__=="__main__":
         if area<20:
             continue
         print area
-        ob_area_list.append(area)
+        word_area_list.append(area)
         word_label_list.append(mask_value)
-        img_bin_words[labeled_array==mask_value]=img_bin[labeled_array==mask_value]
-    #hist(ob_area_list)
-    area_mode = stats.mode(ob_area_list,axis=None)
+        word_label_array[labeled_array==mask_value]=labeled_array[labeled_array==mask_value]
+    #hist(word_area_list)
+    area_mode = stats.mode(word_area_list,axis=None)
     print area_mode
 
+    if showFigure:
+        figure(); gray(); # don't use colors 
+        imshow(word_label_array)
+        jet()
+        show()
     #print img_bin,stats.mode(img_bin,axis=None)
     #print img_bin,np.max(img_bin)
 
@@ -67,37 +82,41 @@ if __name__=="__main__":
     #img_bin = filters.maximum_filter(img_bin,7)
     #img_bin = filter.threshold_adaptive(img_bin,7)
 
-    #img_bin[img_bin>0]=255
-    #Image.fromarray(uint8(img_bin)).save('feature_points.png')
-
-    #====== extract the centroids of word regions=====  
-    # list of slice index of object's box
-    obj_list = measurements.find_objects(labeled_array)
+    # === list of slice index of object's box
+    obj_list = measurements.find_objects(word_label_array)
     print 'num of objs:', len(obj_list) 
     print 'num of words:', len(word_label_list)
+    word_centroid_y_list =[]
+    word_centroid_x_list =[]
     for i in word_label_list:
         word = obj_list[i-1]
-        y = (word[0].stop-word[0].start)/2 
-        x = (word[1].stop-word[1].start)/2 
-        print x,y
+        y = (word[0].stop+word[0].start)/2 
+        x = (word[1].stop+word[1].start)/2 
 
-    figure(); gray(); # don't use colors 
+        word_centroid_y_list.append(y)
+        word_centroid_x_list.append(x)
+        #print x,y
 
-    # show the two pics  on 1*2 frame
-    #subplot(1,3,1)   
-    imshow(img_gray)
-    #subplot(1,3,2)
-    figure(); gray(); # don't use colors 
-    imshow(img_bin)
-    figure(); gray(); # don't use colors 
-    imshow(img_bin_words)
-    #subplot(1,3,3)
-    #imshow(labeled_array)
-    #ob = labeled_array[obj_list[100]]
-    figure(); gray(); # don't use colors 
-    imshow(labeled_array)
+        #h = ob[0].stop-ob[0].start
+        #w = ob[1].stop-ob[1].start
+        #print ob, h, w
+    return (word_centroid_y_list, word_centroid_x_list)
 
-    # starts the figure GUI and raises the figure windows
+if __name__=="__main__":
+    #file_name='./data/das-0.jpg'
+    #file_name='./data/EngBill21.jpg'
+    #file_name='./data/sample1.jpg'
+    file_name='/Users/xcbfreedom/projects/data/formula_images/user_images/531283fa24f0b8afb.png'
+    # load the image file
+    img = Image.open(file_name)
+    figure(); 
+    imshow(img)
+    show()
 
-    jet()
+    #====== extract word regions and their centroids=====  
+    y_list, x_list= get_word_centroid_points(img)
+
+    figure(); 
+    imshow(img)
+    plot(x_list,y_list,'r*')
     show()
